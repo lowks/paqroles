d933cee24399ab89409491646eac6b44532ce494
Ivan Kleshnin <ivan@paqmind.com>
2014-09-08 22:39:42 +0300

    Change LICENSE to MIT

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'PaqRoles',
     version = '1.0.0.dev',
-    license = 'Creative Commons Attribution-Noncommercial-Share Alike license',
+    license = 'MIT',
     packages = find_packages(),
     include_package_data = True,
 )
